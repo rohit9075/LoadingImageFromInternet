6d692e3af76d946edf268380450e69aa8ea8d8a4
rohit9075 <rohit9075@gmail.com>
2018-02-17 20:49:48 +0530

    Initial commit

---
+++ app/src/main/java/rohit/com/loadingimagefrominternet/LoadImage.java
@@ -0,0 +1,15 @@
+package rohit.com.loadingimagefrominternet;
+
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
+
+/**
+ * Created by VIVEK on 11/8/2017.
+ */
+
+public class LoadImage extends AsyncTask<String,Void,Bitmap> {
+    @Override
+    protected Bitmap doInBackground(String... strings) {
+        return null;
+    }
+}
